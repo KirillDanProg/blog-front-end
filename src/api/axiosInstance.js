c980b8cb32a5c00ebf144c061d8873b8f0ab5b1f
KirillDanProg <kirill2898v@gmail.com>
2022-12-06 00:15:16 +0300

    api url added

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: "http://localhost:4444",
+    baseURL: "https://blog-api-erlu.onrender.com",
     headers: {
         "Authorization": `Bearer ${window.localStorage.getItem("token")}`
     }
